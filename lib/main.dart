1654c4bc8a364b7c611864118be0cdb5ff677506
Hwang Hyejung <jimin_ma@naver.com>
2021-05-19 15:03:45 +0900

    Main top 완성

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primaryColor: Color(0xFF601d54),
+        primaryColor: Color(0xFF571e4d),
+        scaffoldBackgroundColor: Color(0xFF571e4d),
+        accentColor: Colors.yellowAccent,
+        textTheme: TextTheme(bodyText2: TextStyle(color: Colors.white)),
       ),
       home: MyHomePage(),
     );
@@ -31,6 +34,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0,
         leading: Image(image: AssetImage('images/logo.png')),
         title: Container(
           height: 36,
@@ -42,7 +46,8 @@ class _MyHomePageState extends State<MyHomePage> {
                 style: OutlinedButton.styleFrom(
                   primary: Colors.white,
                   side: BorderSide(color: Colors.white, width: 2),
-                  shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                  shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0))),
                   textStyle: TextStyle(
                     fontSize: 18,
                   ),
@@ -50,13 +55,87 @@ class _MyHomePageState extends State<MyHomePage> {
                 onPressed: () {},
                 child: Text('Fly'),
               ),
-              Text('Sleep', style: TextStyle(fontSize: 17, color: Color(0xFFdddddd)),),
-              Text('Eat', style: TextStyle(fontSize: 17, color: Color(0xFFdddddd)),),
+              Text(
+                'Sleep',
+                style: TextStyle(fontSize: 17, color: Color(0xFFdddddd)),
+              ),
+              Text(
+                'Eat',
+                style: TextStyle(fontSize: 17, color: Color(0xFFdddddd)),
+              ),
             ],
           ),
         ),
       ),
-      body: Container(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            margin: EdgeInsets.fromLTRB(20, 0, 20, 8),
+            width: MediaQuery
+                .of(context)
+                .size
+                .width * 0.9,
+            color: Color(0xFF6d185e),
+            child: ListTile(
+              leading: Icon(Icons.person, color: Colors.white, size: 30,),
+              horizontalTitleGap: 0,
+              title: Text(
+                '1 Adult, Economy',
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(20, 0, 20, 8),
+            width: MediaQuery
+                .of(context)
+                .size
+                .width * 0.9,
+            color: Color(0xFF6d185e),
+            child: ListTile(
+              leading: Icon(Icons.location_on, color: Colors.white, size: 30,),
+              horizontalTitleGap: 0,
+              title: Text(
+                'Seoul, South Korea',
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(20, 0, 20, 8),
+            width: MediaQuery
+                .of(context)
+                .size
+                .width * 0.9,
+            color: Color(0xFF6d185e),
+            child: ListTile(
+              leading: Icon(Icons.airplanemode_active, color: Color(0xFFF7D4F1), size: 30,),
+              horizontalTitleGap: 0,
+              title: Text(
+                'Choose Destination',
+                style: TextStyle(fontSize: 18, color: Color(0xFFF7D4F1)),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(20, 0, 20, 8),
+            width: MediaQuery
+                .of(context)
+                .size
+                .width * 0.9,
+            color: Color(0xFF6d185e),
+            child: ListTile(
+              leading: Icon(Icons.calendar_today, color: Colors.white, size: 30,),
+              horizontalTitleGap: 0,
+              title: Text(
+                'Jan 13 - Jan 30',
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
